cc905134bc12320be82c4ac3411ab87d4a1f48e9
ECC <echristeleit@gmail.com>
2016-02-19 10:28:30 -0500

    added dotenv and mongoUri paths

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-'use strict';
+"use strict";
 
-var express = require('express'),
-    mongoose = require('mongoose'),
-    routes = require('./app/routes/index.js');
+var express = require("express"),
+    mongoose = require("mongoose"),
+    routes = require("./app/routes/index.js");
 
 var app = express();
+require("dotenv").load();
 
-var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/test';
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost/test";
 
-mongoose.connect('mongodb://localhost/test', function(err) {
+mongoose.connect(mongoUri, function(err) {
     if(err) {
-        console.log('connection error', err);
+        console.log("connection error", err +" on "+mongoUri);
     } else {
-        console.log('connection successful');
+        console.log("connection to "+mongoUri+" successful");
     }
-});;
+});
 
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/models', express.static(process.cwd() + '/app/models'));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use("/controllers", express.static(process.cwd() + "/app/controllers"));
+app.use("/models", express.static(process.cwd() + "/app/models"));
 
-routes(app)
+routes(app);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.listen(port,function(){
-  console.log('Listening on port '+port+'...')
-})
+  console.log("Listening on port "+port+"...")
+});
